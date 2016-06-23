36cbe936f78ec7140b30885168996968e9d60aa1
mati <mrkukawski@gmail.com>
2016-06-23 23:27:16 +0200

    formularze druciarstwo

--- intervals/app.py
+++ intervals/app.py
@@ -1,23 +1,21 @@
 import os
+
 from flask import Flask, render_template, request
 from werkzeug import secure_filename
-from intervals.tempo import get_data
 
-from intervals.forms import GpxForm, ParamsForm
+from intervals.forms import GpxForm
+from intervals.tempo import get_data
+from intervals.utils import split_list
 
 BASE_DIR = os.path.dirname(os.path.os.path.abspath(__file__))
 UPLOADS_DIR = os.path.join(BASE_DIR, 'uploads/')
 STATIC_DIR = os.path.join(BASE_DIR, 'static/')
 sup = STATIC_DIR
-print STATIC_DIR
 
-# static_url_path="static", static_folder="static"
-
-# app = Flask(__name__, static_folder=STATIC_DIR, static_url_path=STATIC_DIR)
 # local
-app = Flask(__name__, static_url_path=sup)
+# app = Flask(__name__, static_url_path=sup)
 # serwer
-# app = Flask(__name__, static_folder=STATIC_DIR)
+app = Flask(__name__, static_folder=STATIC_DIR)
 app.secret_key = 's3cr3t'
 
 
@@ -25,78 +23,19 @@ app.secret_key = 's3cr3t'
 def hello_world():
     return render_template('index.html')
 
-@app.route('/upload/', methods=('GET', 'POST'))
+
+@app.route('/upload/')
 def upload():
     form = GpxForm()
-    form_params = ParamsForm()
-    if form.validate_on_submit():
-        filename = secure_filename(form.gpx.data.filename)
-        upload_path = os.path.join(UPLOADS_DIR, filename)
-        form.gpx.data.save(upload_path)
-    else:
-        filename = None
     return render_template('upload.html', form=form)
 
+
 @app.route('/run/', methods=('GET', 'POST'))
-def make_plots():
-    if request.method == 'POST':
-        fname = request.form['gpx']
-        # accuracyPaceDiffForIntervalSearch = request.form['accuracyPaceDiffForIntervalSearch']
-        # print accuracyPaceDiffForIntervalSearch
-        ccuracy_liczba_probek = int(request.form['ccuracy_liczba_probek'])
-        accuracyPaceDiffForIntervalSearch = int(request.form['accuracyPaceDiffForIntervalSearch'])
-        point_distance_minimal_between_interwalsS = int(request.form['point_distance_minimal_between_interwalsS'])
-        filter_orderS = int(request.form['filter_orderS'])
-        wiekS = int(request.form['wiekS'])
-        zakresyTetnaProcentyS = int(request.form['zakresyTetnaProcentyS'])
-
-        print accuracyPaceDiffForIntervalSearch, type(accuracyPaceDiffForIntervalSearch)
-        print point_distance_minimal_between_interwalsS
-        print filter_orderS
-        print wiekS
-        print zakresyTetnaProcentyS
-        zakresyTetnaProcentyS = [54, 63, 73, 81, 91, 100]
-
-
-
-        fpath = os.path.join(UPLOADS_DIR, fname)
-        print 'fpath', fpath
-        raw_input('??????????????????????????????')
-        base_name, print_list = get_data(
-            STATIC_DIR,
-            fpath,
-            ccuracy_liczba_probek,
-            accuracyPaceDiffForIntervalSearch,
-            point_distance_minimal_between_interwalsS,
-            filter_orderS,
-            wiekS,
-            zakresyTetnaProcentyS,
-            #
-            # output_dir,
-            # fileName,
-            # ccuracy_liczba_probek,
-            # accuracy_pace_diff_for_interval_search,
-            # point_distance_minimal_between_interwals,
-            # filter_order,
-            # wiek,
-            # zakresyTetnaProcenty
-
-
-        )
-        plot_path = os.path.join(STATIC_DIR, base_name[:-3] + 'png')
-        print 'plot_path', plot_path
-        if os.path.isfile(plot_path):
-            return render_template('plots.html', plot_path=base_name, data_list=print_list)
-        else:
-            return 'nie ma'
-    else:
-        return render_template('plots.html')
-
-
-@app.route('/test/', methods=('GET', 'POST'))
 def test_web():
     if request.method == 'POST':
         form = GpxForm()
+        if not form.validate_on_submit():
+            return 'zjebales, jeszcze raz'
         filename = secure_filename(form.gpx.data.filename)
         upload_path = os.path.join(UPLOADS_DIR, filename)
         form.gpx.data.save(upload_path)
@@ -105,13 +44,8 @@ def test_web():
         point_distance_minimal_between_interwalsS = int(request.form['point_distance_minimal_between_interwalsS'])
         filter_orderS = int(request.form['filter_orderS'])
         wiekS = int(request.form['wiekS'])
-        zakresyTetnaProcentyS = [int(x) for x in request.form['zakresyTetnaProcentyS'].split()]
-
-        # filename = secure_filename(form.gpx.data.filename)
-        # upload_path = os.path.join(UPLOADS_DIR, filename)[:-3]+'png'
-        # plot_path = os.path.join(STATIC_DIR, base_name[:-3] + 'png')
+        zakresyTetnaProcentyS = split_list(request.form['zakresyTetnaProcentyS'])
         fpath = os.path.join(UPLOADS_DIR, filename)
-
         base_name, print_list = get_data(
             STATIC_DIR,
             fpath,
@@ -121,23 +55,12 @@ def test_web():
             filter_orderS,
             wiekS,
             zakresyTetnaProcentyS,
-            #
-            # output_dir,
-            # fileName,
-            # ccuracy_liczba_probek,
-            # accuracy_pace_diff_for_interval_search,
-            # point_distance_minimal_between_interwals,
-            # filter_order,
-            # wiek,
-            # zakresyTetnaProcenty
-
         )
         plot_path = os.path.join(STATIC_DIR, base_name)
         if os.path.isfile(plot_path):
             return render_template('plots.html', plot_path=base_name, data_list=print_list, shitt=plot_path)
         else:
             return 'nie ma'
-    # return render_template('plots.html')
 
 if __name__ == '__main__':
    app.debug = True
--- intervals/intervals/forms.py
+++ intervals/intervals/forms.py
@@ -23,77 +23,32 @@ class GpxForm(Form):
             validators.DataRequired(),
 
         ],
-        default=10)
+        default=1)
     point_distance_minimal_between_interwalsS = IntegerField(
-        'accuracyPaceDiffForIntervalSearch',
+        'point_distance_minimal_between_interwalsS',
         [
             validators.DataRequired(),
 
         ],
-        default=1)
+        default=10)
     filter_orderS = IntegerField(
-        'ccuracy_liczba_probek',
+        'filter_orderS',
         [
             validators.DataRequired(),
 
         ],
         default=5)
     wiekS = IntegerField(
-        'ccuracy_liczba_probek',
+        'wiekS',
         [
             validators.DataRequired(),
 
         ],
         default=30)
     zakresyTetnaProcentyS = StringField(
-        'ccuracy_liczba_probek',
+        'zakresyTetnaProcentyS',
         [
             validators.DataRequired(),
 
         ],
-        default="8 54 63 73 81 91 100")
-
-# CCURACY_LICZBA_PROBEK = 8  # ile kolejnych probek ma byc wieksze (dla szukania startu) badz mniejsze (dla stopu) od probki aktualnie iterowanej
-# accuracyPaceDiffForIntervalSearch = 1  # jakiej roznicy tempa (min / km) ma szukac
-# point_distance_minimal_between_interwalsS = 10  # dla ilu kolejnych punktow olewac start / stop interwalu (jezeli jest 'dlugi rozped')
-# filter_orderS = 5  # liczba probek do usrednienia, dla latwiejszego wykrywania startu / stopu
-# # usredniam dane z x probek, przebieg robi sie mniej poszarpany, na wykresie sa wartosci oryginalne
-# wiekS = 30
-# zakresyTetnaProcentyS = [54, 63, 73, 81, 91, 100]
-class ParamsForm(Form):
-    ccuracy_liczba_probek = IntegerField(
-        'ccuracy_liczba_probek',
-        [
-            validators.DataRequired(),
-
-         ])
-    accuracyPaceDiffForIntervalSearch = IntegerField(
-        'accuracyPaceDiffForIntervalSearch',
-        [
-            validators.DataRequired(),
-
-        ])
-    point_distance_minimal_between_interwalsS = IntegerField(
-        'accuracyPaceDiffForIntervalSearch',
-        [
-            validators.DataRequired(),
-
-        ])
-    filter_orderS = IntegerField(
-        'ccuracy_liczba_probek',
-        [
-            validators.DataRequired(),
-
-        ])
-    wiekS = IntegerField(
-        'ccuracy_liczba_probek',
-        [
-            validators.DataRequired(),
-
-        ])
-    zakresyTetnaProcentyS = IntegerField(
-        'ccuracy_liczba_probek',
-        [
-            validators.DataRequired(),
-
-        ])
+        default="54 63 73 81 91 100")
